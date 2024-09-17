8b78d9dde9cf82562683acd84e0e7002d494a648
Han-Ni Lin <u6788552@anu.edu.au>
2024-09-17 22:30:11 +1000

    update modules

--- polyBERT/do_tokenize.py
+++ polyBERT/do_tokenize.py
@@ -3,14 +3,18 @@ import argparse
 from transformers import DebertaV2Tokenizer
 from datasets import load_dataset
 
+"""Global vairables"""
+tokenizer = None
 original_pretrain_file = 'generated_polymer_smiles_train'
 
 
 def tokenize(data):
+    global tokenizer
     res = tokenizer(data['text'])
     return {'input_ids': res['input_ids']}
 
 def main():
+    global tokenizer
     # Create an ArgumentParser object
     parser = argparse.ArgumentParser()
 
@@ -22,12 +26,12 @@ def main():
 
     # Loads the tokenizer
     tokenizer = DebertaV2Tokenizer.from_pretrained(f"./spm_{size}.model")
-    dataset = load_dataset('text', data_files={'train': f'{original_pretrain_file}_{size}.txt',
-                                'test': 'generated_polymer_smiles_dev.txt'},
+    dataset = load_dataset('text', data_files={'train': f'data/{original_pretrain_file}_{size}.txt',
+                                'test': 'data/generated_polymer_smiles_dev.txt'},
                                 cache_dir=f'dataset_cache_{size}/')
 
     dataset = dataset.map(tokenize, batched=True, batch_size=10_000, num_proc=10) 
-    dataset.save_to_disk(f'dataset_tokenized_{size}')
+    dataset.save_to_disk(f'data/tokenized_{size}')
 
 if __name__ == '__main__':
     main()
